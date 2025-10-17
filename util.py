e73e744b58fd4510048fec0307a962d0d9b03d16
thuhollow2 <131473776+thuhollow2@users.noreply.github.com>
2025-10-18 02:19:40 +0800

    Update util.py

--- util.py
+++ util.py
@@ -124,6 +124,11 @@ def check_time2(time_dict):
     current_time = datetime.now(tz).time()
     return current_time >= given_time
 
+def fmt_num(v):
+    s = format(float(v)/100.0, f".15f")
+    s = s.rstrip("0").rstrip(".")
+    return s or "0"
+
 def format_json_to_text(json_data, list_data):
     index_data = []
     if not json_data:
@@ -135,11 +140,11 @@ def format_json_to_text(json_data, list_data):
         text_result += "-"*20 + "\n"
         body = problem_info.get('body', '未知问题')
         problemType = {1:"单选题", 2:"多选题", 3:"投票题", 4:"填空题", 5:"主观题"}.get(problem_info['problemType'], "其它题型")
-        text_result += f"PPT: 第{problem_info['index']}页 {problemType} {problem_info.get('score', 0)}分\n问题: {body}\n"
+        text_result += f"PPT: 第{problem_info['index']}页 {problemType} {fmt_num(problem_info.get('score', 0))}分\n问题: {body}\n"
         if 'options' in problem_info:
             for option in problem_info['options']:
                 text_result += f"- {option['key']}: {option['value']}\n"
-        text_result += f"答案: 暂无, 待生成\n"
+        text_result += f"答案: 暂无\n"
     text_result += "已解锁问题:\n"
     if not index_data:
         text_result += "无\n"
